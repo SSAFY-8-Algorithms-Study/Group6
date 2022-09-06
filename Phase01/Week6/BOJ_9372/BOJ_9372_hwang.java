4269fa7fdf46be3772e1714f8f9de61678ee6671
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2022-09-06 22:40:28 +0900

    [ADD] 황정주 BOJ 9372 풀이 제출

---
+++ Phase01/Week6/BOJ_9372/BOJ_9372_hwang.java
@@ -0,0 +1,42 @@
+package day0904;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_9372_hwang {
+	
+	static int N, M, cnt;
+	static int[][] map;
+	static boolean[] visit;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			map = new int[N+1][N+1];
+			
+			for(int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				
+				map[from][to] = 1;
+			} // end input;
+			
+			
+			System.out.println(N-1);
+			
+		}
+	}
+
+}
