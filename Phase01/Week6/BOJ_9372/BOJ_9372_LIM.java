f7eb7c705c493f89cd759a885b13382bfb100bc4
임경찬 <rudcks5562@naver.com>
2022-09-14 03:39:20 +0900

    [ADD] week6 임경찬

---
+++ Phase01/Week6/BOJ_9372/BOJ_9372_LIM.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		StringBuilder sb= new StringBuilder();
+		int TC= sc.nextInt();
+		
+		
+		for(int t=1;t<=TC;t++) {
+			
+			int n=sc.nextInt();
+			int m=sc.nextInt();
+			
+			for(int M=0;M<m;M++) {
+				
+				int temp=sc.nextInt();
+				int temp2=sc.nextInt();
+				
+			}
+			
+			sb.append(n-1+"\n");
+			
+			
+			
+			
+			
+			
+		}
+		System.out.println(sb);
+		
+		
+
+	}
+
+}
